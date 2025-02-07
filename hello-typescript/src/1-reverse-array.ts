9a67d95ea8de1b35a74ec4faac213af0970e3176
Morteza Mirzaei <dev.mirzaei@gmail.com>
2025-02-07 21:33:24 +0330

    [RESOLVE] ex1 issues

--- hello-typescript/src/1-reverse-array.ts
+++ hello-typescript/src/1-reverse-array.ts
@@ -1,8 +1,8 @@
 export function reverseArray<T>(items: T[]): T[] {
     const reversedArray: T[] = [];
 
-    for (let i: number = 0; i < items.length; ++i) {
-        reversedArray[i] = items[items.length - i - 1];
+    for (let i = items.length - 1; i <= 0; ++i) {
+        reversedArray.push(items[i])
     }
 
     return reversedArray;
