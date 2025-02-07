a0a7af1dd977d8398203c50fc034f24bddeb1862
Morteza Mirzaei <dev.mirzaei@gmail.com>
2025-02-07 22:19:50 +0330

    [TEST] add test for functions

--- hello-typescript/src/main.ts
+++ hello-typescript/src/main.ts
@@ -1,5 +1,51 @@
-async function main(): Promise<void> {
-  console.log("Hello, friend!");
+import { reverseArray } from "./1-reverse-array";
+import { countNumbersInArray } from "./2-count-numbers-in-array";
+import { sortObjects } from "./3-sort-objects";
+
+function testReverseArray(): void {
+  const items = ["red", true, "JavaScript", 1.36];
+  const result = reverseArray(items);
+  console.log(result);
 }
 
-main().then();
+function testCountNumbersInArray(): void {
+  const items = ["3", 58, 46, null, true, { id: 12 }];
+  const result = countNumbersInArray(items);
+  console.log(result);
+}
+
+function testSortObjects(): void {
+  const objects = [
+    {
+      id: 1,
+      createdAt: "2025-02-01T00:00:00.000Z",
+      modifiedAt: "2025-02-02T00:00:00.000Z",
+    },
+    {
+      id: 2,
+      name: "Morteza",
+      createdAt: "2025-01-01T00:00:00.000Z",
+      modifiedAt: "2025-02-29T00:00:00.000Z",
+    },
+    {
+      id: 3,
+      createdAt: "2025-01-15T00:00:00.000Z",
+    },
+    {
+      id: 4,
+      color: "red",
+      createdAt: "2025-01-29T00:00:00.000Z",
+      modifiedAt: "2025-02-07T00:00:00.000Z",
+    },
+  ];
+  const result = sortObjects(objects);
+  console.log(result);
+}
+
+function main(): void {
+  testReverseArray();
+  testCountNumbersInArray();
+  testSortObjects();
+}
+
+main();
